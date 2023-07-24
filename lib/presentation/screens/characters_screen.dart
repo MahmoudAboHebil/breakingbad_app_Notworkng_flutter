29c8f987483a248d5bcce9cbeabe6cc2feffe2fb
Mahmoud <mahmoudabohebil@gmail.com>
2023-07-24 10:29:48 +0300

    Establishing The Cubit in characters_screen.dart and getting data from it.

--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -1,14 +1,83 @@
+import 'package:bloc_with_breakingbad_api/business_logic/cubit/characters_cubit.dart';
+import 'package:bloc_with_breakingbad_api/constants/my_colors.dart';
+import 'package:bloc_with_breakingbad_api/presentation/widgets/character_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/src/rendering/sliver_grid.dart';
+import '../../data/models/characters.dart';
 
-class CharactersScreen extends StatelessWidget {
+class CharactersScreen extends StatefulWidget {
+  const CharactersScreen({super.key});
+
+  @override
+  State<CharactersScreen> createState() => _CharactersScreenState();
+}
+
+class _CharactersScreenState extends State<CharactersScreen> {
+  late List<Character> allCharacters;
+  @override
+  void initState() {
+    super.initState();
+    allCharacters =
+        BlocProvider.of<CharactersCubit>(context).getAllCharacters();
+  }
+
+  Widget buildBlocWidget() {
+    return BlocBuilder<CharactersCubit, CharactersState>(
+      builder: (context, state) {
+        if (state is CharactersLoaded) {
+          allCharacters = (state).characters;
+          return buildLoadedListWidgets();
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              color: MyColors.myYellow,
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  Widget buildLoadedListWidgets() {
+    return SingleChildScrollView(
+      child: Container(
+        color: MyColors.myGrey,
+        child: Column(
+          children: [
+            GridView.builder(
+              itemBuilder: (context, index) {
+                // not done
+                return CharacterItem();
+              },
+              shrinkWrap: true,
+              physics: const ClampingScrollPhysics(),
+              padding: EdgeInsets.zero,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 2 / 3,
+                  crossAxisSpacing: 1,
+                  mainAxisSpacing: 1),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Charcters screeeeeeeeeeeeeen'),
+      appBar: AppBar(
+        backgroundColor: MyColors.myYellow,
+        title: Text(
+          'Characters',
+          style: TextStyle(
+            color: MyColors.myGrey,
+          ),
         ),
       ),
+      body: buildBlocWidget(),
     );
   }
 }
